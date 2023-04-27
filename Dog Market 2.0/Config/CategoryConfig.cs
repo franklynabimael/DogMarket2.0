70ebe8951ccf10af9fb2d775414e8b69885c34d2
Franklin Hernandez <franklynabimael@gmail.com>
2023-04-27 19:38:26 -0400

    Data seed Category

--- Dog Market 2.0/Config/CategoryConfig.cs
+++ Dog Market 2.0/Config/CategoryConfig.cs
@@ -6,9 +6,24 @@ namespace Dog_Market_2._0.Config;
 
 public class CategoryConfig : IEntityTypeConfiguration<Category>
 {
+    private const int CategoryId = 1;
+    private const int CategoryId2 = 2;
     public void Configure(EntityTypeBuilder<Category> builder)
     {
         builder.HasKey(x => x.Id);
         builder.Property(x => x.Id).ValueGeneratedOnAdd();
+
+        builder.HasData(new Category()
+        {
+            Id = CategoryId,
+            Name = "Juguetes",
+            Description = "Son Juguetes",
+        });
+        builder.HasData(new Category()
+        {
+            Id = CategoryId2,
+            Name = "Comida",
+            Description = "Lo que comen los perros",
+        });
     }
 }
